8226ddfb688e53da50ffe3946dfd5d8163f00df3
soleribbon <ivar202@gmail.com>
2024-04-05 16:39:44 +0200

    Initial Commit

---
+++ MuseumBeacon/MuseumBeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumBeaconApp.swift
+//  MuseumBeacon
+//
+//  Created by Ravi Heyne on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MuseumBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
