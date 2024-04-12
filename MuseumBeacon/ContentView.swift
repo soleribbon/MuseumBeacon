7b230dd1b70e60c3e0cdbf241fc0a5b699084bc7
soleribbon <ivar202@gmail.com>
2024-04-12 15:13:16 +0200

    First commit

--- MuseumBeacon/ContentView.swift
+++ MuseumBeacon/ContentView.swift
@@ -1,24 +1,63 @@
-//
-//  ContentView.swift
-//  MuseumBeacon
-//
-//  Created by Ravi Heyne on 05/04/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var detector = BeaconDetector()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            switch detector.proximityDescription {
+            case "Searching...":
+                searchingView
+            default:
+                detectedRoomView
+            }
         }
         .padding()
+        .animation(.easeInOut, value: detector.proximityDescription)
     }
-}
 
-#Preview {
-    ContentView()
+    private var searchingView: some View {
+        VStack {
+            Spacer()
+
+            Text("Searching for beacons...")
+                .font(.title)
+                .foregroundColor(.gray)
+                .padding()
+            ProgressView()
+                .controlSize(.large)
+                .progressViewStyle(CircularProgressViewStyle(tint: .black))
+            Spacer()
+        }
+    }
+
+    private var detectedRoomView: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Detected Room")
+                .font(.headline)
+                .foregroundColor(.secondary)
+            Text(detector.proximityDescription)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(determineColorBasedOnProximity())
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(white: 0.95))
+        .cornerRadius(12)
+        .shadow(radius: 10)
+    }
+
+    private func determineColorBasedOnProximity() -> Color {
+        switch detector.proximityDescription {
+        case let description where description.contains("Very close"):
+            return .green
+        case let description where description.contains("Near"):
+            return .orange
+        case let description where description.contains("Far"):
+            return .red
+        default:
+            return .black
+        }
+    }
 }
