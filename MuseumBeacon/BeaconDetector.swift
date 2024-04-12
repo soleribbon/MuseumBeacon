374aa39193a4a653a26cf14d30451a83c9bff5d9
soleribbon <ivar202@gmail.com>
2024-04-12 15:12:48 +0200

    first commit

---
+++ MuseumBeacon/BeaconDetector.swift
@@ -0,0 +1,8 @@
+//
+//  BeaconDetector.swift
+//  MuseumBeacon
+//
+//  Created by Ravi Heyne on 05/04/24.
+//
+
+import Foundation
---
+++ MuseumBeacon/BeaconSetup.swift
@@ -0,0 +1,8 @@
+//
+//  BeaconSetup.swift
+//  MuseumBeacon
+//
+//  Created by Ravi Heyne on 12/04/24.
+//
+
+import Foundation
